8468b5e586568ee4f55afc943f7b0a332bfc7e1d
gch12345 <1274021602@qq.com>
2019-04-19 21:52:07 +0800

    1

---
+++ test/test/test.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 0, n = 0;
+//for (i = 100; i < 200; i++)
+//{
+//	int t = 0;
+//	for (n = 2; n < i; n++)
+//	{
+//		if (i%n == 0){
+//			t = 1;
+//			break;
+//		}
+//	}
+//	if(t == 0) printf("%d\t", i); 
+//		
+//}
+//	system("pause");
+//return 0;
+//}
+//int main()
+//{
+//	int i = 0, k = 0, h = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (k = 1; k <= 9; k++)
+//		{
+//			h = i*k;
+//			printf("%d*%d\t", i, k);
+//		}
+//	}
+//
+//	return 0;
+//}
+//int main(){
+//	int i = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (((i % 4 == 0) && (i % 100 !=0)) ||( i % 400 == 0))
+//			printf("%d\t", i);
+//	}
+//	return 0;
+//}
