789b2d32b3bdbdb6f2331b70ff860cb56fe1fa91
gch12345 <1274021602@qq.com>
2019-05-06 20:59:21 +0800

    布斯算法

--- test/test/test.c
+++ test/test/test.c
@@ -606,356 +606,555 @@
 //}
 //
 //（运算器）
-int add(int a,int b)//加法
-{
-	int re = a;
-	while (b)
-	{
-		int tmp = a;
-		a = a^b;
-		b = (tmp&b) << 1;
-		re = a;
+//int add(int a,int b)//加法
+//{
+//	int re = a;
+//	while (b)
+//	{
+//		int tmp = a;
+//		a = a^b;
+//		b = (tmp&b) << 1;
+//		re = a;
+//	}
+//	return re;
+//}
+//int reduce(int a,int b)//减法
+//{
+//	b = add(~b, 1);
+//	return add(a, b);
+//}
+//void change(int a)
+//{
+//	int arr[9] = { 0 };
+//	int i = 8;
+//	int j = 0;
+//	while (1)
+//	{
+//		if (a < 0)
+//		{
+//			a = -a;
+//			j = 1;
+//		}
+//		arr[i] = a % 2;
+//		i = i - 1;
+//		a = a / 2;
+//		if (a == 0)
+//		{
+//			break;
+//		}
+//		else if (a < 2)
+//		{
+//			arr[i] = 1;
+//			break;
+//		}
+//	}
+//	if (j == 1)
+//	{
+//		arr[0] = 1;
+//	}
+//	for (i = 0; i < 9;i++)
+//	{	
+//		printf("%d", arr[i]);
+//	}
+//
+//	printf("\n");
+//
+//}
+//void change0(int a,int b)//不带进位右移
+//{
+//	int arr[9] = { 0 };
+//	int arr0[9] = { 0 };
+//	int i = 8;
+//	int j = 0;
+//	while (1)
+//	{
+//		if (a < 0)
+//		{
+//			a = -a;
+//			j = 1;
+//		}
+//		arr[i] = a % 2;
+//		i = i - 1;
+//		a = a / 2;
+//		if (a == 0)
+//		{
+//			break;
+//		}
+//		else if (a < 2)
+//		{
+//			arr[i] = 1;
+//			break;
+//		}
+//	}
+//	if (j == 1)
+//	{
+//		arr[0] = 1;
+//	}
+//	j = 0;
+//	for (i = 9-b; i<9; i++)
+//	{
+//		arr0[j] = arr[i];
+//		j++;
+//	}
+//	for (i = 0; i < 9 - b; i++)
+//	{
+//		arr0[j] = arr[i];
+//		j++;
+//	}
+//	for (j = 0; j < 9; j++)
+//	{
+//		printf("%d", arr0[j]);
+//	}
+//	printf("\n");
+//}
+//void change1(int a, int b)//带进位循环右移
+//{
+//	int arr[9] = { 0 };
+//	int arr0[9] = { 0 };
+//	int i = 8;
+//	int j = 0;
+//	while (1)
+//	{
+//		if (a < 0)
+//		{
+//			a = -a;
+//			j = 1;
+//		}
+//		arr[i] = a % 2;
+//		i = i - 1;
+//		a = a / 2;
+//		if (a == 0)
+//		{
+//			break;
+//		}
+//		else if (a < 2)
+//		{
+//			arr[i] = 1;
+//			break;
+//		}
+//	}
+//	if (j == 1)
+//	{
+//		arr0[0] = 1;
+//	}
+//	j = 1;
+//	
+//	for (i = 9 - b; i<9; i++)
+//	{
+//		arr0[j] = arr[i];
+//		j++;
+//	}
+//	for (i = 1; i < 9 - b; i++)
+//	{
+//		arr0[j] = arr[i];
+//		j++;
+//	}
+//	for (j = 0; j < 9; j++)
+//	{
+//		printf("%d", arr0[j]);
+//	}
+//	printf("\n");
+//}
+//void change2(int a, int b)
+//{
+//	int arr[9] = { 0 };
+//	int arr0[9] = { 0 };
+//	int i = 8;
+//	int j = 0;
+//	while (1)
+//	{
+//		if (a < 0)
+//		{
+//			a = -a;
+//			j = 1;
+//		}
+//		arr[i] = a % 2;
+//		i = i - 1;
+//		a = a / 2;
+//		if (a == 0)
+//		{
+//			break;
+//		}
+//		else if (a < 2)
+//		{
+//			arr[i] = 1;
+//			break;
+//		}
+//	}
+//	if (j == 1)
+//	{
+//		arr0[0] = 1;
+//	}
+//
+//	j = 1;
+//	for (i = b+1; i < 9; i++)
+//	{
+//		arr0[j] = arr[i];
+//		j++;
+//	}
+//	for (i = 1; i<b+1; i++)
+//	{
+//		arr0[j] = arr[i];
+//		j++;
+//	}
+//	for (j = 0; j < 9; j++)
+//	{
+//		printf("%d", arr0[j]);
+//	}
+//	printf("\n");
+//}
+//int multiply(int a, int b)
+//{
+//	int i = 0;
+//	int j = 0;
+//	if (a < 0)
+//	{
+//		a = add(~a, 1);
+//		i = 1;
+//	}
+//	if (b < 0)
+//	{
+//		b = add(~b, 1);
+//		j = 1;
+//	}
+//	int ans = 0;
+//	while (b)
+//	{
+//		if (b & 1)
+//		ans = add(ans, a);
+//		a = a << 1;
+//		b = b >> 1;
+//	}
+//	if (i^j)
+//		ans=add(~ans, 1);
+//	return ans;
+//}
+//int division(int a, int b)
+//{
+//	int i = 0;
+//	int j = 0;
+//	if (a < 0)
+//	{
+//		a = add(~a, 1);
+//		i = 1;
+//	}
+//	if (b < 0)
+//	{
+//		b = add(~b, 1);
+//		j = 1;
+//	}
+//	int res = 0;
+//	while (a >= b)
+//	{
+//		res = add(res, 1);
+//		a = reduce(a, b);
+//	}
+//	if (i^j)
+//		res = add(~res, 1);
+//	return res;
+//}
+//int main()
+//{
+//	int op=0 ;
+//	int a = -1;
+//	int b = 1;
+//	int f = 0;
+//	int p = 1;
+//	int CN = 0;
+//	int FC = 0;	
+//	while (p)
+//	{
+//		printf("输入操作类型:");
+//		scanf("%d", &op);
+//		switch (op)
+//		{
+//		case 0000:
+//			printf("请输入第一个值:");
+//			scanf("%d", &a);
+//			printf("\n");
+//			printf("二进制序列为:");
+//			change(a);
+//			break;
+//		case 0001:
+//			printf("请输入第二个值:");
+//			scanf("%d", &b);
+//			printf("\n");
+//			printf("二进制序列为:");
+//			change(b);
+//			break;
+//		case 10:
+//			printf("输出AB相与\n");
+//			f = a&b;
+//			change(f);
+//			break;
+//		case 11:
+//			printf("ab相或\n");
+//			f = a | b;
+//			change(f);
+//			break;
+//		case 100:
+//			printf("对a取反\n");
+//			f = ~a;
+//			change(f);
+//			break;
+//		case 101:
+//			printf("对a不带进位循环右移b（取低3位）位\n");
+//			change0(a, b);
+//			break;
+//		case 110:
+//			printf("输入CN的值\n");
+//			printf("0.a逻辑右移一位\n");
+//			printf("1.a带进位循环右移一位\n");
+//			scanf("%d", &CN);
+//			if (CN)
+//				change1(a, 1);
+//			else
+//			{
+//				f = a >> 1;
+//				change(f);
+//			}
+//			break;
+//		case 111:
+//			printf("输入CN的值\n");
+//			printf("0.a逻辑左移一位\n");
+//			printf("1.a带进位循环左移一位\n");
+//			scanf("%d", &CN);
+//			if (CN)
+//				change2(a, 1);
+//			else
+//			{
+//				f = a << 1;
+//				change(f);
+//			}
+//			break;
+//		case 1000:
+//			printf("输入FC的值");
+//			scanf("%d", FC);
+//			break;
+//		case 1001:
+//			printf("a+b的值\n");
+//			add(a, b);
+//			break;
+//		case 1010:
+//			printf("a-b的值\n");
+//			change(reduce(a, b));
+//			break;
+//		case 1011:
+//			printf("a*b的值\n");
+//			change(multiply(a, b));
+//			break;
+//		case 1100:
+//			printf("a/b的值\n");
+//			change(division(a, b));
+//			break;
+//		case 1111:
+//			printf("退出程序\n");
+//			p = 0;
+//			break;
+//		default:
+//			printf("输入错误\n");
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+int a = 0;//x的位数
+int b = 0;//y的位数
+void print(char Y[]) {//输出乘数数组
+	for (int i = 0; i < b + 1; ++i) {
+		if (i == 1) {
+			printf(".");
+		}
+		printf("%c", Y[i]);
 	}
-	return re;
 }
-int reduce(int a,int b)//减法
-{
-	b = add(~b, 1);
-	return add(a, b);
+void complement(char x[], int m, int n) {//根据已求得的原码数组求补码码
+	for (int i = n + 1; i < m + 1; ++i) {//取反码
+		if (x[i] == '1') {
+			x[i] = '0';
+		}
+		else {
+			x[i] = '1';
+		}
+	}
+	for (int j = m; j > n; --j) {//取补码
+		if (x[j] == '1') {
+			x[j] = '0';
+			continue;
+		}
+		x[j] = '1';
+		break;
+	}
 }
-void change(int a)
-{
-	int arr[9] = { 0 };
-	int i = 8;
-	int j = 0;
-	while (1)
-	{
-		if (a < 0)
-		{
-			a = -a;
-			j = 1;
-		}
-		arr[i] = a % 2;
-		i = i - 1;
-		a = a / 2;
-		if (a == 0)
-		{
-			break;
-		}
-		else if (a < 2)
-		{
-			arr[i] = 1;
-			break;
-		}
+void complement1(int num, char x[], int m) {//求被乘数和负的被乘数的补码
+	if (num >= 0) {
+		x[0] = '0';
+		x[1] = '0';
 	}
-	if (j == 1)
-	{
-		arr[0] = 1;
-	}
-	for (i = 0; i < 9;i++)
-	{	
-		printf("%d", arr[i]);
-	}
+	else {
+		x[0] = '1';
+		x[1] = '1';
+		num = -num;
 
-	printf("\n");
+	}
+	x[2] = '.';
+	//求原码
+	for (int i = 2 + m; i > 2; --i) {
+		x[i] = num % 2 + '0';
+		num /= 2;
+	}
+	if (x[0] == '1') {
+		complement(x, m + 2, 2);//求补码
+	}
+	printf("%s\n", x);
+}
 
-}
-void change0(int a,int b)//不带进位右移
-{
-	int arr[9] = { 0 };
-	int arr0[9] = { 0 };
-	int i = 8;
-	int j = 0;
-	while (1)
-	{
-		if (a < 0)
-		{
-			a = -a;
-			j = 1;
-		}
-		arr[i] = a % 2;
-		i = i - 1;
-		a = a / 2;
-		if (a == 0)
-		{
-			break;
-		}
-		else if (a < 2)
-		{
-			arr[i] = 1;
-			break;
-		}
+void complement2(int num, char x[], int m) {//求乘数补码
+	if (num >= 0) {
+		x[0] = '0';
 	}
-	if (j == 1)
-	{
-		arr[0] = 1;
-	}
-	j = 0;
-	for (i = 9-b; i<9; i++)
-	{
-		arr0[j] = arr[i];
-		j++;
-	}
-	for (i = 0; i < 9 - b; i++)
-	{
-		arr0[j] = arr[i];
-		j++;
-	}
-	for (j = 0; j < 9; j++)
-	{
-		printf("%d", arr0[j]);
-	}
-	printf("\n");
-}
-void change1(int a, int b)//带进位循环右移
-{
-	int arr[9] = { 0 };
-	int arr0[9] = { 0 };
-	int i = 8;
-	int j = 0;
-	while (1)
-	{
-		if (a < 0)
-		{
-			a = -a;
-			j = 1;
-		}
-		arr[i] = a % 2;
-		i = i - 1;
-		a = a / 2;
-		if (a == 0)
-		{
-			break;
-		}
-		else if (a < 2)
-		{
-			arr[i] = 1;
-			break;
-		}
-	}
-	if (j == 1)
-	{
-		arr0[0] = 1;
-	}
-	j = 1;
-	
-	for (i = 9 - b; i<9; i++)
-	{
-		arr0[j] = arr[i];
-		j++;
-	}
-	for (i = 1; i < 9 - b; i++)
-	{
-		arr0[j] = arr[i];
-		j++;
-	}
-	for (j = 0; j < 9; j++)
-	{
-		printf("%d", arr0[j]);
-	}
-	printf("\n");
-}
-void change2(int a, int b)
-{
-	int arr[9] = { 0 };
-	int arr0[9] = { 0 };
-	int i = 8;
-	int j = 0;
-	while (1)
-	{
-		if (a < 0)
-		{
-			a = -a;
-			j = 1;
-		}
-		arr[i] = a % 2;
-		i = i - 1;
-		a = a / 2;
-		if (a == 0)
-		{
-			break;
-		}
-		else if (a < 2)
-		{
-			arr[i] = 1;
-			break;
-		}
-	}
-	if (j == 1)
-	{
-		arr0[0] = 1;
-	}
+	else {
+		x[0] = '1';
+		num = -num;
 
-	j = 1;
-	for (i = b+1; i < 9; i++)
-	{
-		arr0[j] = arr[i];
-		j++;
 	}
-	for (i = 1; i<b+1; i++)
-	{
-		arr0[j] = arr[i];
-		j++;
+	//求原码
+	for (int i = m; i > 0; --i) {
+		x[i] = num % 2 + '0';
+		num /= 2;
 	}
-	for (j = 0; j < 9; j++)
-	{
-		printf("%d", arr0[j]);
+	if (x[0] == '1') {
+		complement(x, m, 0);//求补码
 	}
+	print(x);
 	printf("\n");
 }
-int multiply(int a, int b)
-{
-	int i = 0;
-	int j = 0;
-	if (a < 0)
-	{
-		a = add(~a, 1);
-		i = 1;
+void print2(char x[], int m) {//输出结果的原码形式
+	int xy = 0;
+	if (x[0] == '1') {
+		complement(x, m, 0);//求补码
+		printf("-");
 	}
-	if (b < 0)
-	{
-		b = add(~b, 1);
-		j = 1;
+	int i = 3;
+	while (x[i] == '0') {
+		++i;
 	}
-	int ans = 0;
-	while (b)
-	{
-		if (b & 1)
-		ans = add(ans, a);
-		a = a << 1;
-		b = b >> 1;
+	int count = 0;
+	for (int j = i; j < m + 1; ++j) {
+		printf("%c", x[j]);
 	}
-	if (i^j)
-		ans=add(~ans, 1);
-	return ans;
+	printf("\n");
+	printf("转换为10进制为:");
+	if (x[0] == '1') {
+		printf("-");
+	}
+	for (int j = i; j < m + 1; ++j) {
+		if (x[j] == '1') {
+			xy += (int)pow(2, m - j);
+		}
+	}
+	printf("%d\n", xy);
 }
-int division(int a, int b)
-{
-	int i = 0;
-	int j = 0;
-	if (a < 0)
-	{
-		a = add(~a, 1);
-		i = 1;
-	}
-	if (b < 0)
-	{
-		b = add(~b, 1);
-		j = 1;
-	}
-	int res = 0;
-	while (a >= b)
-	{
-		res = add(res, 1);
-		a = reduce(a, b);
-	}
-	if (i^j)
-		res = add(~res, 1);
-	return res;
-}
-int main()
-{
-	int op=0 ;
-	int a = -1;
-	int b = 1;
-	int f = 0;
-	int p = 1;
-	int CN = 0;
-	int FC = 0;	
-	while (p)
-	{
-		printf("输入操作类型:");
-		scanf("%d", &op);
-		switch (op)
-		{
-		case 0000:
-			printf("请输入第一个值:");
-			scanf("%d", &a);
-			printf("\n");
-			printf("二进制序列为:");
-			change(a);
-			break;
-		case 0001:
-			printf("请输入第二个值:");
-			scanf("%d", &b);
-			printf("\n");
-			printf("二进制序列为:");
-			change(b);
-			break;
-		case 10:
-			printf("输出AB相与\n");
-			f = a&b;
-			change(f);
-			break;
-		case 11:
-			printf("ab相或\n");
-			f = a | b;
-			change(f);
-			break;
-		case 100:
-			printf("对a取反\n");
-			f = ~a;
-			change(f);
-			break;
-		case 101:
-			printf("对a不带进位循环右移b（取低3位）位\n");
-			change0(a, b);
-			break;
-		case 110:
-			printf("输入CN的值\n");
-			printf("0.a逻辑右移一位\n");
-			printf("1.a带进位循环右移一位\n");
-			scanf("%d", &CN);
-			if (CN)
-				change1(a, 1);
-			else
-			{
-				f = a >> 1;
-				change(f);
+void Add(char Z[], char X[]) {//补码数组相加
+	for (int i = a + 2; i >= 0; --i) {
+		if (i == 2) {
+			continue;
+		}
+		if (Z[i] + X[i] - '0' == '2') {
+			Z[i] = '0';
+			for (int j = i - 1; j >= 0; --j) {
+				if (Z[j] == '0') {
+					Z[j] = '1';
+					break;
+				}
+				else if (Z[j] == '1') {
+					Z[j] = '0';
+				}
 			}
-			break;
-		case 111:
-			printf("输入CN的值\n");
-			printf("0.a逻辑左移一位\n");
-			printf("1.a带进位循环左移一位\n");
-			scanf("%d", &CN);
-			if (CN)
-				change2(a, 1);
-			else
-			{
-				f = a << 1;
-				change(f);
-			}
-			break;
-		case 1000:
-			printf("输入FC的值");
-			scanf("%d", FC);
-			break;
-		case 1001:
-			printf("a+b的值\n");
-			add(a, b);
-			break;
-		case 1010:
-			printf("a-b的值\n");
-			change(reduce(a, b));
-			break;
-		case 1011:
-			printf("a*b的值\n");
-			change(multiply(a, b));
-			break;
-		case 1100:
-			printf("a/b的值\n");
-			change(division(a, b));
-			break;
-		case 1111:
-			printf("退出程序\n");
-			p = 0;
-			break;
-		default:
-			printf("输入错误\n");
-			break;
+		}
+		else {
+			Z[i] = Z[i] + X[i] - '0';
 		}
 	}
+}
+void Booth(int x, int y) {//
+	char X[36] = { '\0' };
+	char Y[34] = { '\0' };
+	char _X[36] = { '\0' };
+	char Z[36] = { '\0' };
+	for (int i = 0; i < a + 3; ++i) {
+		if (i == 2) {
+			Z[i] = '.';
+			continue;
+		}
+		Z[i] = '0';
+	}
+	printf("%d补码为:", x);
+	complement1(x, X, a);
+	printf("%d补码为:", y);
+	complement2(y, Y, b);
+	printf("%d补码为:", -x);
+	complement1(-x, _X, a);
+	char yb_1 = '0';
+	printf("\n");
+	for (int i = 0; i < b + 1; ++i) {
+		if (Y[b] == yb_1) {
+		}
+		else if (Y[b] < yb_1) {
+			Add(Z, X);
+			printf("                    +X\n%s\n", X);
+		}
+		else {
+			Add(Z, _X);
+			printf("                   +_X\n%s\n", _X);
+		}
+		printf("%s  ", Z);
+		print(Y);
+		printf("  %c\n", yb_1);
+		if (i < b) {//Y[]与Z[]右移
+			printf("右移\n");
+			yb_1 = Y[b];
+			for (int i = b; i > 0; --i) {
+				Y[i] = Y[i - 1];
+			}
+			Y[0] = Z[a + 2];
+			for (int i = a + 2; i > 0; --i) {
+				if (i == 3) {
+					Z[i] = Z[i - 2];
+					continue;
+				}
+				if (i == 2) {
+					continue;
+				}
+				Z[i] = Z[i - 1];
+			}
+			printf("%s  ", Z);
+			print(Y);
+			printf("  %c\n", yb_1);
+		}
+	}
+	char XY[70] = { '\0' };
+	strcpy(XY, Z);
+	for (int i = a + 3; i < a + 3 + b; ++i) {
+		XY[i] = Y[i - a - 3];
+	}
+	printf("x*y的补码为%s\n", XY);
+	print2(XY, a + b + 2);
+}
+void main() {
+	int x, y, n;
+	scanf("%d %d", &x, &y);
+	n = x;
+	while (n) {
+		++a;
+		n /= 2;
+	}
+	n = y;
+	while (n) {
+		++b;
+		n /= 2;
+	}
+	Booth(x, y);
 	system("pause");
-	return 0;
 }
