5817f7e8e2e84daeee6cff14e407eb6abf95b3df
gch12345 <1274021602@qq.com>
2019-05-22 22:29:49 +0800

    扫雷

--- task_day09/task_day09/game.h
+++ task_day09/task_day09/game.h
@@ -4,9 +4,9 @@
 #include<time.h>
 #include<string.h>
 
-#define ROW 12
-#define COL 12
-#define NUM 90
+#define ROW 12//扫雷盘的长度
+#define COL 12//扫雷盘的宽度
+#define NUM 20//雷的个数
 void printfgame(char arr[ROW][COL], int row, int col);
 void initarr(char arr[ROW][COL], char a, int row, int col);
 void setmine(char arr[ROW][COL], int row, int col ,int num);
