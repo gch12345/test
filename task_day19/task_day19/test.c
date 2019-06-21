de854d36c56b908bcc58d7c9ea475af431a1af16
gch12345 <1274021602@qq.com>
2019-06-21 13:31:45 +0800

    提交代码

--- task_day19/task_day19/test.c
+++ task_day19/task_day19/test.c
@@ -122,42 +122,92 @@
 //	return 0;
 //}
 
-void * Mymemmove(void * dst, const void * src, int count)
-{
-	assert(dst != NULL&&src != NULL);
-	char* p = (char*)dst;
-	if ((char*)dst >= (char*)src + count || (char*)src >= (char*)dst)
-	{
-		while (count)
-		{
-			*(char*)dst = *(char*)src;
-			((char*)dst)++;
-			((char*)src)++;
-			count--;
-		}
-	}
-	else
-	{
-		(char*)dst = (char*)dst + count - 1;
-		(char*)src = (char*)src + count - 1;
-		while (count)
-		{
-			*(char*)dst = *(char*)src;
-			((char*)dst)--;
-			((char*)src)--;
-			count--;	
-		}
-	}
-	return p;
-}
+//void * Mymemmove(void * dst, const void * src, int count)
+//{
+//	assert(dst != NULL&&src != NULL);
+//	char* p = (char*)dst;
+//	if ((char*)dst >= (char*)src + count || (char*)src >= (char*)dst)
+//	{
+//		while (count)
+//		{
+//			*(char*)dst = *(char*)src;
+//			((char*)dst)++;
+//			((char*)src)++;
+//			count--;
+//		}
+//	}
+//	else
+//	{
+//		(char*)dst = (char*)dst + count - 1;
+//		(char*)src = (char*)src + count - 1;
+//		while (count)
+//		{
+//			*(char*)dst = *(char*)src;
+//			((char*)dst)--;
+//			((char*)src)--;
+//			count--;	
+//		}
+//	}
+//	return p;
+//}
+//
+//int main()
+//{
+//	char str1[20] = "abcdefg";
+//	char* str2 = "efgh";
+//	//printf("%s", memmove(str1, str1 + 2, 4));
+//    printf("%s", Mymemmove(str1, str1 + 2, 4));
+//	system("pause");
+//	return 0;
+//}
+//int cmp(void* p1, void* p2)
+//{
+//	assert(p1 != NULL&&p2 != NULL);
+//	return (*(int*)p1 - *(int*)p2);
+//}
+//void swap(void*p1, void*p2, int size)
+//{
+//	assert(p1 != NULL&&p2 != NULL);
+//	int i = 0;
+//	while (size)
+//	{
+//		char temp = *((char*)p1 + i);
+//		*((char*)p1 + i) = *((char*)p2 + i);
+//		*((char*)p2 + i) = temp;
+//		i++;
+//		size--;
+//	}
+//}
+//void Mybubblsort(void* prt, int count, int size, int(*cmp)(void* p1, void* p2))
+//{
+//	assert(prt != NULL&&cmp != NULL);
+//	for (int i = 0; i < count; i++)
+//	{
+//		for (int j = 0; j < count - i - 1; j++)
+//		{
+//			if (cmp((char*)prt + j*size, (char*)prt + (j + 1)*size)>0)
+//			{
+//				swap((char*)prt + j*size, (char*)prt + (j + 1)*size, size);
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0, 24, 3, 1, 5, 78, 3, 35, 6, 7 };
+//	Mybubblsort(arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), cmp);
+//	int le = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0; i < le; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	system("pause");
+//	return 0;
+//}
 
-int main()
+void Myqsort(void *ptr, int count, int size, int(*comp)(const void *, const void *))
 {
-	char str1[20] = "abcdefg";
-	char* str2 = "efgh";
-	//printf("%s", memmove(str1, str1 + 2, 4));
-    printf("%s", Mymemmove(str1, str1 + 2, 4));
-	system("pause");
-	return 0;
+
 }
 
