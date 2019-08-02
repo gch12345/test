02b20f3cedd10cce6749ca64b2191396f71e0b58
gch12345 <1274021602@qq.com>
2019-08-02 21:52:04 +0800

    提交代码

---
+++ holiday_08/holiday_08/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int f(int n)
+//{
+//	static int i = 1;
+//	if (n >= 5)
+//		return n;
+//	n = n + i;
+//	i++;
+//	return f(n);
+//}
+//int main()
+//{
+//	printf("%d\n", f(1));
+//	system("pause");
+//	return 0;
+//}
+//int func()
+//{
+//	int i, j, k = 0;
+//	for (i = 0, j = -1; j = 0; i++, j++)
+//		k++;
+//	return k;
+//}
+//int main()
+//{
+//	printf("%d\n", func());
+//	system("pause");
+//	return 0;
+//}
