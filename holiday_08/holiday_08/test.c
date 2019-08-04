4b594fe40c75176b7525d93f12100d1bf2d8eb97
gch12345 <1274021602@qq.com>
2019-08-04 21:56:50 +0800

    提交代码

--- holiday_08/holiday_08/test.c
+++ holiday_08/holiday_08/test.c
@@ -30,45 +30,115 @@
 //	system("pause");
 //	return 0;
 //}
-char* fun(char* str1, char* str2)
+//char* fun(char* str1, char* str2)
+//{
+//	assert(str1 != NULL&&str2 != NULL);
+//	char*p = str1;
+//	char*p0 = str1;
+//	while (*str1!='\0')
+//	{
+//		int i = 0;
+//		while (*(str2 + i) != '\0')
+//		{
+//			if (*str1 != *(str2 + i))
+//			{
+//				i++;
+//			}
+//			else
+//			{
+//				break;
+//			}
+//		}
+//		if (*(str2 + i) == '\0')
+//		{
+//			*p = *str1;
+//			p++;
+//			str1++;
+//		}
+//		else
+//		{
+//			str1++;
+//		}
+//	}
+//	*p = '\0';
+//	return p0;
+//}
+//int main()
+//{
+//	char arr0[] = "They are students.";
+//	char arr1[] = "aeiou";
+//	char* str = fun(arr0, arr1);
+//	printf("%s\n", str);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[] = { 6, 7, 8, 9, 10 };
+//	int*p = arr;
+//	*(p++) += 123;
+//	printf("%d,\n", *p);
+//	system("pause");
+//	return 0;
+//}
+//int fun(int *arr, int n)
+//{
+//	assert(arr != NULL);
+//	int *p1= arr;
+//	int *p2 = arr;
+//	if (n % 2 == 0)
+//	{
+//		for (int i = 0; i < n / 2; i++)
+//		{
+//			p1 = arr + n / 2 - 1 - i;
+//			p2 = arr + n / 2  + i;
+//			if (*p2 != *p1)
+//			{
+//				return -1;
+//			}
+//		}
+//		return 1;
+//	}
+//	else
+//	{
+//		for (int i = 0; i < n / 2; i++)
+//		{
+//			p1 = arr + n / 2 - 1 - i;
+//			p2 = arr + n / 2 + 1 + i;
+//			if (*p2 != *p1)
+//			{
+//				return -1;
+//			}
+//		}
+//		return 1;
+//	}
+//} 
+//int main()
+//{
+//	int arr[1] = {0};
+//	printf("%d",fun(arr, sizeof(arr)/sizeof(int)));
+//	system("pause");
+//	return 0;
+//}
+int fun(int k)
 {
-	assert(str1 != NULL&&str2 != NULL);
-	char*p = str1;
-	char*p0 = str1;
-	while (*str1!='\0')
+	int sum = 0;
+	int count = k;
+	while (count > 0)
 	{
-		int i = 0;
-		while (*(str2 + i) != '\0')
-		{
-			if (*str1 != *(str2 + i))
-			{
-				i++;
-			}
-			else
-			{
-				break;
-			}
-		}
-		if (*(str2 + i) == '\0')
-		{
-			*p = *str1;
-			p++;
-			str1++;
-		}
-		else
-		{
-			str1++;
-		}
+		sum = sum * 10 + count % 10;
+		count = count / 10;
 	}
-	*p = '\0';
-	return p0;
+	if (sum == k)
+	{
+		return 1;
+	}
+	return 0;
 }
 int main()
 {
-	char arr0[] = "They are students.";
-	char arr1[] = "aeiou";
-	char* str = fun(arr0, arr1);
-	printf("%s\n", str);
+	int k = 1231;
+	printf("%d", fun(k));
 	system("pause");
 	return 0;
 }
