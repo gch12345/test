9dfc466c2790653d4dae5e51ae4324c6ac455a5e
gch12345 <1274021602@qq.com>
2019-08-22 19:09:49 +0800

    提交代码

--- holiday_12/holiday_12/test.c
+++ holiday_12/holiday_12/test.c
@@ -114,146 +114,173 @@
 //}
 
 // Definition for singly-linked list.
- struct ListNode {
-     int val;
-     struct ListNode *next;
- };
+ //struct ListNode {
+ //    int val;
+ //    struct ListNode *next;
+ //};
 
 
 
- struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
- {
-	 struct ListNode*p1 = l1;
-	 struct ListNode*p2 = l2;
-	 struct ListNode*p3 = l1;
-	 struct ListNode*p4 = l1;
-	 int count = 0;
-	 while (p1 != NULL&&p2 != NULL)
-	 {
-		 int num = p1->val + p2->val + count;
-		 p1->val = num % 10;
-		 if (num >= 10)
-		 {
-			 count = 1;
-		 }
-		 else
-		 {
-			 count = 0;
-		 }
-		 p3 = p1;
-		 p4 = p2;
-		 p2->val = p1->val;
-		 p1 = p1->next;
-		 p2 = p2->next;
-	 }
-	 int start = -1;
-	 while (count == 1)
-	 {
-		 if (count == 1)
-		 {
-			 int num = 0;
-			 if (p1 == NULL&&p2 != NULL)
-			 {
-				 num = p2->val + count;
-				 p2->val = num % 10;
-				 start = 0;
-				 p4 = p2;
-				 p2 = p2->next;
-				 if (num >= 10)
-				 {
-					 count = 1;
-				 }
-				 else
-				 {
-					 count = 0;
-				 }
-			 }
-			 else if (p1 != NULL&&p2 == NULL)
-			 {
-				 num = p1->val + count;
-				 p1->val = num % 10;
-				 start = 1;
-				 p3 = p1;
-				 p1 = p1->next;
-				 if (num >= 10)
-				 {
-					 count = 1;
-				 }
-				 else
-				 {
-					 count = 0;
-				 }
-			 }
-		 }
+ //struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+ //{
+	// struct ListNode*p1 = l1;
+	// struct ListNode*p2 = l2;
+	// struct ListNode*p3 = l1;
+	// struct ListNode*p4 = l1;
+	// int count = 0;
+	// while (p1 != NULL&&p2 != NULL)
+	// {
+	//	 int num = p1->val + p2->val + count;
+	//	 p1->val = num % 10;
+	//	 if (num >= 10)
+	//	 {
+	//		 count = 1;
+	//	 }
+	//	 else
+	//	 {
+	//		 count = 0;
+	//	 }
+	//	 p3 = p1;
+	//	 p4 = p2;
+	//	 p2->val = p1->val;
+	//	 p1 = p1->next;
+	//	 p2 = p2->next;
+	// }
+	// int start = -1;
+	// while (count == 1)
+	// {
+	//	 if (count == 1)
+	//	 {
+	//		 int num = 0;
+	//		 if (p1 == NULL&&p2 != NULL)
+	//		 {
+	//			 num = p2->val + count;
+	//			 p2->val = num % 10;
+	//			 start = 0;
+	//			 p4 = p2;
+	//			 p2 = p2->next;
+	//			 if (num >= 10)
+	//			 {
+	//				 count = 1;
+	//			 }
+	//			 else
+	//			 {
+	//				 count = 0;
+	//			 }
+	//		 }
+	//		 else if (p1 != NULL&&p2 == NULL)
+	//		 {
+	//			 num = p1->val + count;
+	//			 p1->val = num % 10;
+	//			 start = 1;
+	//			 p3 = p1;
+	//			 p1 = p1->next;
+	//			 if (num >= 10)
+	//			 {
+	//				 count = 1;
+	//			 }
+	//			 else
+	//			 {
+	//				 count = 0;
+	//			 }
+	//		 }
+	//	 }
 
-		 if (count == 1 && p1 == NULL&&p2 == NULL)
-		 {
-			 struct ListNode*p5 = (struct ListNode*)malloc(sizeof(struct ListNode));
-			 p5->val = 1;
-			 p5->next = NULL;
-			 if (start == 0)
-			 {
-				 p4->next = p5;
-				 return l2;
-			 }
-			 else
-			 {
-				 p3->next = p5;
-				 return l1;
-			 }
-		 }
-	 }
-	 if (start == -1)
-	 {
-		 if (p1 == NULL)
-			 return l2;
-		 else
-		 {
-			 return l1;
-		 }
-	 }
-	 else if (start == 0)
-	 {
-		 return l2;
-	 }
-	 else
-	 {
-		 return l1;
-	 }
- }
- int lengthOfLongestSubstring(char * s){
-	 int count = 0;
-	 int re = 0;
-	 char arr[256] = { 0 };
-	 char*p = s;
-	 int c = 0;
-	 while (*s != '\0')
-	 {
-		 if (arr[*s] == 1)
-		 {
-			 int i = 1;
-			 arr[*s] = 0;
-			 while (*s != *(s - i))
-			 {
-				 arr[*(s - i)] = 0;
-				 i++;
-			 }
-			 s = s - i + 1;
-			 count = 0;
+	//	 if (count == 1 && p1 == NULL&&p2 == NULL)
+	//	 {
+	//		 struct ListNode*p5 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	//		 p5->val = 1;
+	//		 p5->next = NULL;
+	//		 if (start == 0)
+	//		 {
+	//			 p4->next = p5;
+	//			 return l2;
+	//		 }
+	//		 else
+	//		 {
+	//			 p3->next = p5;
+	//			 return l1;
+	//		 }
+	//	 }
+	// }
+	// if (start == -1)
+	// {
+	//	 if (p1 == NULL)
+	//		 return l2;
+	//	 else
+	//	 {
+	//		 return l1;
+	//	 }
+	// }
+	// else if (start == 0)
+	// {
+	//	 return l2;
+	// }
+	// else
+	// {
+	//	 return l1;
+	// }
+ //}
+ //int lengthOfLongestSubstring(char * s){
+	// int count = 0;
+	// int re = 0;
+	// char arr[256] = { 0 };
+	// char*p = s;
+	// int c = 0;
+	// while (*s != '\0')
+	// {
+	//	 if (arr[*s] == 1)
+	//	 {
+	//		 int i = 1;
+	//		 arr[*s] = 0;
+	//		 while (*s != *(s - i))
+	//		 {
+	//			 arr[*(s - i)] = 0;
+	//			 i++;
+	//		 }
+	//		 s = s - i + 1;
+	//		 count = 0;
 
-			 continue;
-		 }
-		 else
-		 {
-			 arr[*s] = 1;
-			 s++;
-			 count++;
-		 }
+	//		 continue;
+	//	 }
+	//	 else
+	//	 {
+	//		 arr[*s] = 1;
+	//		 s++;
+	//		 count++;
+	//	 }
 
-		 if (re<count)
-		 {
-			 re = count;
-		 }
-	 }
-	 return re;
- }
+	//	 if (re<count)
+	//	 {
+	//		 re = count;
+	//	 }
+	// }
+	// return re;
+ //}
+
+int lengthOfLongestSubstring(char * s)
+{
+	int arr[256] = { 0 };
+	int first = 1;
+	int len = 0;
+	int num = 1;
+	int max_len = 0;
+	while (*s != '\0')
+	{
+		if (arr[*s] != 0)
+		{
+			if (first <= arr[*s])
+			{
+				len = num - first;
+				first = arr[*s] + 1;
+				max_len = (max_len>len) ? max_len : len;
+			}
+		}
+		arr[*s] = num;
+		num++;
+		s++;
+	}
+	len = num - first;
+	max_len = (max_len>len) ? max_len : len;
+	return max_len;
+}
